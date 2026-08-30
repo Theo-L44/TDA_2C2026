aa6a7a6844839ca1f7dc7b1ad20ce08e9902886b
Theo <theoluis44@gmail.com>
2026-08-30 20:12:40 -0300

    empecé a escribir el código

---
+++ Taller 1/ej1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main ( String [] args ) {
+        BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+        Scanner scanner = new Scanner (br);
+        PrintWriter printer = new PrintWriter (bw);
+        
+            
+
+        printer.close()
+    }
+}
